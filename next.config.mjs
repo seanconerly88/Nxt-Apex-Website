8ceb3ee2f30250604171de3cf786c51903806fbc
Sean <sean@nxtapexai.com>
2026-08-24 13:46:37 -0500

    Redirect /portfolio to /reviews (fix 404)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  async redirects() {
+    return [
+      { source: '/portfolio', destination: '/reviews', permanent: true },
+    ];
+  },
 };
 
 export default nextConfig;
